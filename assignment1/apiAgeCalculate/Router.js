994f8312d757d217e69eab16dcb0b06388ec0a4a
Shruti <shrutiperiwal21@gmail.com>
2022-07-26 19:43:26 +0530

    assignment

---
+++ assignment1/apiAgeCalculate/Router.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/',(req, res) => {
+    const{year,month,date,name} = req.query;
+    const age = new Date() - new Date(`${year}-${month}-${date}`);
+    const currentAge = Math.floor(age/1000/60/24/365);
+    res.render("HomePage", {name, age: currentAge})
+});
+
+export default router;
